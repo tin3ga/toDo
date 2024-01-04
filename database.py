6358152c01052ae62cd10e4e403685fa2039b19a
tin3ga <118301331+tin3ga@users.noreply.github.com>
2024-01-04 23:04:01 +0300

    feat: Update database models
    - Add a User model
    - Create relationship between User and Todo models.
    - Move database to postgresql

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///./todos.db")
+load_dotenv()
+database_url: str = os.getenv("DATABASE_URL")
+
+
+engine = create_engine(database_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- models.py
+++ models.py
@@ -1,15 +1,46 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey
+from flask_login import UserMixin
+from database import SessionLocal
 
 
 class Base(DeclarativeBase):
     pass
 
 
+class User(Base, UserMixin):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    todos = relationship('Todo', backref='user')
+
+    def __repr__(self) -> str:
+        return f"user={self.user_id}"
+
+    def add_todo(self, task):
+        with SessionLocal() as session:
+            new_task = Todo(task=task, user_id=self.id, completed=False)
+            session.add(new_task)
+            session.commit()
+
+    def task_complete(self, task_id):
+        with SessionLocal() as session:
+            task = session.get(Todo, task_id)
+            task.completed = True
+            session.commit()
+
+    def delete_task(self, task_id):
+        with SessionLocal() as session:
+            task_to_delete = session.get(Todo, task_id)
+            session.delete(task_to_delete)
+            session.commit()
+
+
 class Todo(Base):
     __tablename__ = "todos"
     id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
     task: Mapped[str] = mapped_column(nullable=False)
     completed: Mapped[bool] = mapped_column(nullable=False)
 
-    def __repr__(self) -> str:
-        return f"task_id={self.id}"
+
